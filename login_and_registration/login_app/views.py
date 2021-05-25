001f9cdb754cd721504d83e786f1869b7f319f82
Oscar Pinedo <opinedo19@gmail.com>
2021-05-24 17:50:29 -0700

    added all routes and views with redirection to root until fully implemented

--- login_and_registration/login_app/views.py
+++ login_and_registration/login_app/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import User
 
 # Create your views here.
+def index(request):
+    return render(request, 'login.html')
+
+def register(request):
+    if request.method == "GET":
+        return redirect('/')
+    return redirect('/')
+    
+def login(request):
+    if request.method == "GET":
+        return redirect('/')
+    return redirect('/')
+
+def logout(request):
+    return redirect('/')
+
+def success(request):
+    return redirect('/')
